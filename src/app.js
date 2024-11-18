678af754b1528819a0cd124116e7db47aa1f0974
“Raineecorn” <janlouraine@gmail.com>
2024-11-18 15:48:24 +0800

    Adding vercel.json

--- src/app.js
+++ src/app.js
@@ -1,36 +1,28 @@
-const express = require('express')
-const dotenv = require('dotenv')
-const cors = require('cors')
-const jwt = require('jsonwebtoken')
-dotenv.config()
-const app = express()
+const express = require('express');
+const dotenv = require('dotenv');
+const cors = require('cors');
 
+dotenv.config();
+const app = express();
 
-//configure here
-app.use(cors())
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
-//port used 3000 if invalid make 5000
-const PORT = process.env.PORT || 5000
+// Middleware
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
+// Routes
+app.use('/flashbox/api/tracker', require('./routes/tracker/app'));
+app.use('/flashbox/api/entry', require('./routes/entry/app'));
+app.use('/flashbox/api/account', require('./routes/identification/app'));
 
-
-//global routes
-app.use('/flashbox/api/tracker', require('./routes/tracker/app'))
-app.use('/flashbox/api/entry', require('./routes/entry/app'))
-app.use('/flashbox/api/account', require('./routes/identification/app'))
-
-// Route to check the version
 app.get('/flashbox/api/version', (req, res) => {
-  res.json({ version: "2024.11.02" });
+    res.json({ version: "2024.11.02" });
 });
 
-//return if invalid routes
-app.use(async (req, res) => {
-  res.status(404).send(`Cant Find Routes`);
+// 404 Fallback
+app.use((req, res) => {
+    res.status(404).send('Cannot Find Route');
 });
-//listen port
-app.listen(PORT, () =>
-  console.log(`Now Listening to Port ${PORT}!!`))
 
-module.exports = app
+// Export the app
+module.exports = app;
